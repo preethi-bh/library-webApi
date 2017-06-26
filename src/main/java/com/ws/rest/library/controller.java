bf34cc7cd64e0b9c8ed964a851f72e6f023f5075
AdItYa <preethi.marple@gmail.com>
2017-06-26 17:43:55 +0530

    modified:   src/main/java/com/ws/rest/library/controller.java

--- src/main/java/com/ws/rest/library/controller.java
+++ src/main/java/com/ws/rest/library/controller.java
@@ -6,6 +6,7 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.GenericEntity;
 import javax.ws.rs.core.MediaType;
 
 @Path("/rest")
@@ -15,8 +16,8 @@ public class controller {
 		@GET
 		@Path("/search")
 		@Produces(MediaType.APPLICATION_XML)
-		public  List<Model> getBook(@PathParam("search") String username,@PathParam("search") String subject){
-			List<Model> m=service.getBookAvail(username,subject);
+		public  GenericEntity<List<Model>> getBook(@PathParam("search") String username,@PathParam("search") String subject){
+			GenericEntity<List<Model>> m=(GenericEntity<List<Model>>) service.getBookAvail(username,bname);
 			return m;
 		}
 	}
