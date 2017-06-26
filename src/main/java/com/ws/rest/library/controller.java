b88a724b5f4ada7544a6d37413b2b2bf8c46e6af
AdItYa <preethi.marple@gmail.com>
2017-06-26 21:51:34 +0530

    modified:   src/main/java/com/ws/rest/library/controller.java

--- src/main/java/com/ws/rest/library/controller.java
+++ src/main/java/com/ws/rest/library/controller.java
@@ -7,6 +7,7 @@ import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.QueryParam;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
