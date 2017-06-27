501e606babe2c7de1ea6d17163750a000c72d3a6
AdItYa <preethi.marple@gmail.com>
2017-06-27 16:22:44 +0530

    modified:   src/main/java/com/ws/rest/library/controller.java

--- src/main/java/com/ws/rest/library/controller.java
+++ src/main/java/com/ws/rest/library/controller.java
@@ -6,6 +6,7 @@ import javax.ws.rs.GET;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.FormParam;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.Produces;
